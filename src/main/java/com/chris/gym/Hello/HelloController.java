d91396e28d63b31a0d2425ea1a532d73d8481e73
Chris <ct.choi@outlook.com>
2021-07-05 10:18:04 +0800

    初次

--- src/main/java/com/chris/gym/Hello/HelloController.java
+++ src/main/java/com/chris/gym/Hello/HelloController.java
@@ -20,33 +20,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello")
 public class HelloController {
 
-    private User getUser(){
-
-        // 获取User
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null) {
-//            LOG.debug("no authentication in security context found");
-//            return "no authentication in security context found.";
-            return null;
-        } else {
-            // 获取username
-            String username = "";
-            if (authentication.getPrincipal() instanceof UserDetails) {
-                UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
-                username = springSecurityUser.getUsername();
-            } else if (authentication.getPrincipal() instanceof String) {
-                username = (String) authentication.getPrincipal();
-            }
-            // 获取user
-            User user = userMapper.findByUsername(username);
-
-            return user;
-
-        }
-
-    }
-
 
     @ApiOperation(value = "更新hello")
     @PostMapping("/update")
